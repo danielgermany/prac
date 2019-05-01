89802bf1b95e3d0105071701f5c924cb22c30396
danielgermany <daniel.germany@uamaker.nyc>
2019-05-01 18:01:34 -0400

    updated quesiton 1

--- pracDef.py
+++ pracDef.py
@@ -4,7 +4,5 @@ def askQuestion():
 
 #Questions
 
-def question1(ID):
-    if ID == 1:
-        print("What is your name?");name = input()
-        print("Hello, ", name , ", nice to meet you")
+def question1(name):
+    print("Hello, ", name , ", nice to meet you")
--- pracRun.py
+++ pracRun.py
@@ -2,4 +2,6 @@ from pracDef import *
 
 x = askQuestion()
 
-question1(x)
+if x == 1:
+    print("What is your name?");input = input()
+    question1(input)
